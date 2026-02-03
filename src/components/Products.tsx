2bdf36e7253286f7c1bea5c156cf59a17aa97caa
hemankkumar24 <hemankkumar24@gmail.com>
2026-02-03 22:44:03 +0530

    update: responsive + url
    Fixed fallback url and added basic responsiveness

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -47,7 +47,7 @@ const Products = ({ userName }: ProductsProps) => {
   return (
     <div className='w-full h-full py-5'>
       {/* All the products will be mentioned here */}
-      <div className='grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3'>
         {items.map(item => ( // mapping all the items for every card
           <Card key={item.id} item={item} userName={userName} />
         ))}
--- src/socket.ts
+++ src/socket.ts
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_BACKEND_URL);
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+
+export const socket = io(BACKEND_URL);
